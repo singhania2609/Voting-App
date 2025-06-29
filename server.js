954836d07672f2f8f2ec4b2c1831f67bdc13baf2
unknown <your-email@example.com>
2025-06-29 18:13:33 +0530

    user router updated

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
 
+
+
 // Import the router files
 const userRoutes = require('./routes/userRoutes');
+
 // Use the routers
 app.use('/user',userRoutes);
+
 
 
 // Start the server
