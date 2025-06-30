a59437c666c8b77d01c40aa6d519ef636d5bcf0d
unknown <your-email@example.com>
2025-06-30 11:11:09 +0530

    candidate can vote and count

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -4,7 +4,6 @@ const User=require('../models/user');
 const Candidate= require('./../models/candidate');
 const {jwtAuthMiddleware, generateToken} = require('./../jwt');
 
-
 const chechkAdminRole= async(userID)=>{
     try{
         const user =await User.findById(userID);
@@ -16,6 +15,7 @@ const chechkAdminRole= async(userID)=>{
         return false;
     }
 }
+
 
 // POST route to add candidate
 router.post('/', jwtAuthMiddleware,async (req, res) =>{
@@ -39,6 +39,7 @@ router.post('/', jwtAuthMiddleware,async (req, res) =>{
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
 
 
 router.put('/:candidateId',jwtAuthMiddleware,async (req, res) => {
@@ -65,6 +66,7 @@ router.put('/:candidateId',jwtAuthMiddleware,async (req, res) => {
     }
 });
 
+
 router.delete('/:candidateId', jwtAuthMiddleware,async (req, res) => {
     try {
         if(!chechkAdminRole(req.user.id))
@@ -84,5 +86,80 @@ router.delete('/:candidateId', jwtAuthMiddleware,async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+
+//Let's start voting
+router.post('/vote/:candidateId',jwtAuthMiddleware,async(req,res)=>{
+    //no admin can vote
+    //user can only vote once
+
+    candidateId =req.params.candidateId;
+    userId=req.user.id;
+    try{
+        //Find the candidate document with the specific candidateId
+        const candidate=await Candidate.findById(candidateId);
+        if(!candidate){
+            return res.status(404).json({message: 'Candidate not found'});
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: 'user not found'});
+        }
+        if(user.isVoted){
+            res.status(400).json({message:'You have already voted'});
+        }
+        if(user.role=='admin'){
+            res.status(403).json({message: 'admin is not allowed'});
+        }
+        //Update the Candidate document to record the vote 
+        candidate.votes.push({user: userId})
+        candidate.voteCount++;
+        await candidate.save();
+
+        //update the user document
+        user.isVoted=true;
+        await user.save();
+        res.status(200).json({message: 'vote recoded successfully'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+});
+
+
+//vote count
+router.get('/vote/count',async (req,res)=>{
+    try{
+        //Find all candidates and sort them by voteCount in descending order
+        const candidate=await Candidate.find().sort({voteCount: 'desc'});
+
+        //Map the candidates to only return their name and voteCount
+        const voterRcoded=candidate.map((data)=>{
+            return {
+                party: data.party,
+                count:data.voteCount
+            }
+        })
+
+        return res.status(200).json(voterRcoded)
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+});
+
+
+//candidate list
+router.get('/candidate',async(req,res)=>{
+    try{
+        //list of candidates
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+})
+
 
 module.exports = router;
